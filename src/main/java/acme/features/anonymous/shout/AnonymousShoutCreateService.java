2ecd455c45f5183dfec91171cc903b4fb3732d5f
Miguel Ángel Nieva Arjona <miguenieva2000@gmail.com>
2021-04-18 21:26:52 +0200

    Task S2.8 - Crear Repository Y Service De SpamWord And Shout Spam

--- src/main/java/acme/features/anonymous/shout/AnonymousShoutCreateService.java
+++ src/main/java/acme/features/anonymous/shout/AnonymousShoutCreateService.java
@@ -1,11 +1,18 @@
 package acme.features.anonymous.shout;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Set;
+import java.util.StringTokenizer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import acme.entities.shouts.Shout;
+import acme.entities.spam.Spam;
+import acme.entities.spam.SpamWord;
+import acme.features.anonymous.spam.AnonymousSpamRepository;
 import acme.framework.components.Errors;
 import acme.framework.components.Model;
 import acme.framework.components.Request;
@@ -17,6 +24,9 @@ public class AnonymousShoutCreateService implements AbstractCreateService<Anonym
 	
 	@Autowired
 	protected AnonymousShoutRepository shoutRepository;
+	
+	@Autowired
+	protected AnonymousSpamRepository spamRepository;
 	
 	@Override
 	public boolean authorise(final Request<Shout> request) {
@@ -76,13 +86,48 @@ public class AnonymousShoutCreateService implements AbstractCreateService<Anonym
 		assert request != null;
 		assert entity != null;
 		
+		final StringTokenizer st = new StringTokenizer(entity.getText());
+		
+		List<Spam> spam = new ArrayList<>();
 		Date moment;
 		
 		moment = new Date(System.currentTimeMillis() - 1);
 		entity.setMoment(moment);
+		
+		spam = this.spamRepository.findSpam();
+		
+		 final Set<SpamWord> palabras = spam.get(0).getSpamWords();
+		
+		 final List<SpamWord> palabras1 = new ArrayList<>();
+		 
+		
+		for (final SpamWord x : palabras)
+		     palabras1.add(x);
+		
+		
+		
+		String texto = entity.getText();
+		final Double contador = 0.0;
+		
+		for(int i = 0; i<palabras1.size(); i++) {
+			
+		final String palabra = palabras1.get(i).getWord();
+		
+		
+		while (texto.indexOf(palabra) > -1) {
+		      texto = texto.substring(texto.indexOf(
+		    	  palabra)+ palabra.length(),texto.length());
+		}
+		}
+	
+		
+		if((contador/st.countTokens()) <= spam.get(0).getThreshold()) {
 		this.shoutRepository.save(entity);
+			
+		}
 		
 	}
+	
 
 
 }
