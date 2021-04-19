ac56a7cc2563b783c27ebac213fb8e020b2eef85
Sergio <serrojjim@alum.us,es>
2021-04-19 11:45:55 +0200

    Fix sample-data

--- src/main/java/acme/features/authenticated/task/AuthenticatedTaskCreateService.java
+++ src/main/java/acme/features/authenticated/task/AuthenticatedTaskCreateService.java
@@ -1,65 +1,90 @@
 package acme.features.authenticated.task;
-//package acme.features.anonymous.task;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import acme.entities.task.Task;
-//import acme.features.anonymous.userAccount.AnonymousUserAccountRepository;
-//import acme.framework.components.Errors;
-//import acme.framework.components.Model;
-//import acme.framework.components.Request;
-//import acme.framework.entities.Anonymous;
-//import acme.framework.services.AbstractCreateService;
-//
-//@Service
-//public class AnonymousTaskCreateService implements AbstractCreateService<Anonymous, Task> {
-//
-//	
-//	
-//	@Autowired
-//	protected AnonymousUserAccountRepository repository;
-//
-//	
-//	
-//	@Override
-//	public boolean authorise(final Request<Task> request) {
-//		assert request != null;
-//
-//		return true;
-//	}
-//	
-//	
-//	
-//	@Override
-//	public void bind(final Request<Task> request, final Task entity, final Errors errors) {
-//		assert request != null;
-//		assert entity != null;
-//		assert errors != null;
-//
-//		request.bind(entity, errors);
-//	}
-//
-//	@Override
-//	public void unbind(final Request<Task> request, final Task entity, final Model model) {
-//		
-//	}
-//
-//	@Override
-//	public Task instantiate(final Request<Task> request) {
-//		return null;
-//	}
-//
-//	@Override
-//	public void validate(final Request<Task> request, final Task entity, final Errors errors) {
-//		
-//	}
-//
-//	@Override
-//	public void create(final Request<Task> request, final Task entity) {
-//		
-//	}
-//
-//	
-//
-//}
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.entities.task.Task;
+import acme.framework.components.Errors;
+import acme.framework.components.Model;
+import acme.framework.components.Request;
+import acme.framework.entities.Authenticated;
+import acme.framework.services.AbstractCreateService;
+
+@Service
+public class AuthenticatedTaskCreateService implements AbstractCreateService<Authenticated, Task> {
+	
+	@Autowired
+	protected AuthenticatedTaskRepository taskRepository;
+	
+	@Override
+	public boolean authorise(final Request<Task> request) {
+		assert request != null;
+		
+		return true;
+	}
+	
+	@Override
+	public void bind(final Request<Task> request, final Task entity, final Errors errors) {
+		assert request != null;
+		assert entity != null;
+		assert errors != null;
+		
+		
+		request.bind(entity, errors);
+	}
+	
+	@Override
+	public void unbind(final Request<Task> request, final Task entity, final Model model) {
+		assert request != null;
+		assert entity != null;
+		assert model != null;
+		
+		request.unbind(entity, model, "title", "executionPeriod.finalDate", "executionPeriod.initialDate","workload","description","url");
+	}
+	
+	@Override
+	public Task instantiate(final Request<Task> request) {
+		assert request != null;
+		
+		Task result;
+		Date moment;
+		
+		moment = new Date(System.currentTimeMillis() - 1);
+		
+		result = new Task();
+		result.setTitle("Prueba de instanciar1");
+		result.setDescription("Prueba instanciar1");
+		result.setIsPrivate(false);
+		result.setIsFinished(false);
+		result.setUrl("http://google.es/");
+		result.setLink("http://example5.org");
+		
+		return result;
+		
+	}
+	
+	@Override
+	public void validate(final Request<Task> request, final Task entity, final Errors errors) {
+		assert request != null;
+		assert entity != null;
+		assert errors != null;
+		
+	}
+	
+	@Override
+	public void create(final Request<Task> request, final Task entity) {
+		assert request != null;
+		assert entity != null;
+		
+		Date moment;
+		
+		moment = new Date(System.currentTimeMillis() - 1);
+		entity.setMoment(moment);
+		this.shoutRepository.save(entity);
+		
+	}
+
+
+}
---
+++ src/main/webapp/WEB-INF/views/authenticated/task/addForm.jsp
@@ -0,0 +1,17 @@
+<%@page language="java"%>
+
+<%@taglib prefix="jstl" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@taglib prefix="acme" tagdir="/WEB-INF/tags"%>
+
+<acme:form>
+	<acme:form-textbox code="authenticated.task.form.label.title" path="title"/>
+	<acme:form-textarea code="authenticated.task.form.label.description" path="description"/>
+	<acme:form-textbox code="authenticated.task.form.label.url" path="url"/>
+	<acme:form-checkbox code="authenticated.task.form.label.isFinished" path="isFinished"/>
+	<acme:form-double code="authenticated.task.form.label.workload" path="workload"/>
+	<acme:form-textbox code="authenticated.task.form.label.execution_period_initial_date" path="executionPeriod.initialDate"/>
+	<acme:form-textbox code="authenticated.task.form.label.execution_period_final_date" path="executionPeriod.finalDate"/>
+	
+	<acme:form-submit code="authenticated.task.form.button.create" action="/authenticated/task/create"/>
+	<acme:form-return code="authenticated.task.form.button.return"/>
+</acme:form>
