79aa7f4b1b94d334c9789532ac22caae488faeab
Mariano Manuel Torrado Sánchez <marianomtorrados@gmail.com>
2021-05-16 20:55:07 +0200

    Negative Create test done

--- src/test/java/acme/testing/workplan/ManagerWorkplanTest.java
+++ src/test/java/acme/testing/workplan/ManagerWorkplanTest.java
@@ -100,12 +100,11 @@ public class ManagerWorkplanTest extends AcmeTest {
 	}
 	
 	/**
-	 * Sign in as a manager, create a workplan, list all my workplans, show the recently created one
-	 * and check that every value is correct.
-	 * No errors expected.
+	 * Sign in as a manager, try create a workplan with some erroneous fields
+	 * and check that it has errors, so it wont be created.
 	 */
 	@ParameterizedTest
-	@CsvFileSource(resources = "/workplan/createPositive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@CsvFileSource(resources = "/workplan/createNegative.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(10)
 	void createWorkplanManagerNegative(final int id, final int version, final String execution_period_initial_date, final String execution_period_final_date, final Boolean isPrivate, final String title,
 		final int user_account_id, final String workload) {
@@ -118,19 +117,8 @@ public class ManagerWorkplanTest extends AcmeTest {
 		super.fillInputBoxIn("executionPeriod.finalDate", execution_period_final_date);
 		super.clickOnSubmitButton("Create");
 		
-		super.clickOnMenu("Manager", "List workplans");
-
-		super.checkColumnHasValue(id, 0, title);
-		super.checkColumnHasValue(id, 1, execution_period_final_date);
-		super.checkColumnHasValue(id, 2, execution_period_initial_date);
-		super.checkColumnHasValue(id, 3, "0.00");
+		super.checkErrorsExist();
 		
-		super.clickOnListingRecord(10);
-
-		super.checkInputBoxHasValue("title", title);
-		super.checkInputBoxHasValue("executionPeriod.initialDate", execution_period_initial_date);
-		super.checkInputBoxHasValue("executionPeriod.finalDate", execution_period_final_date);
-
 		super.signOut();
 	}
 	// Ancillary methods ------------------------------------------------------
