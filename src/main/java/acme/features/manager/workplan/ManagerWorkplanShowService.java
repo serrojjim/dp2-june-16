87cd1691052e05dec90742124259cc26c8e1d061
Alvaro <alvarocortescasado@gmail.com>
2021-06-07 13:33:15 +0200

    Updating exec period update workplan

--- src/main/java/acme/features/manager/workplan/ManagerWorkplanShowService.java
+++ src/main/java/acme/features/manager/workplan/ManagerWorkplanShowService.java
@@ -72,10 +72,16 @@ public class ManagerWorkplanShowService implements AbstractShowService<Manager, 
 			model.setAttribute("allTasksAvailable", l);
 		}
 		
+		List<Task> allTasksAlreadySelected = myTasks.stream().filter(x -> x.getWorkplan().contains(entity)).collect(Collectors.toList());
+		
 		model.setAttribute("allTasksAlreadySelected", 
 			myTasks.stream().filter(x -> x.getWorkplan().contains(entity)).collect(Collectors.toList()));
 		
-		model.setAttribute("suggestedExecutionPeriod", entity.getSuggestedExecutionPeriod());
+		model.setAttribute("suggestedExecutionPeriod.initialDate", entity.getSuggestedExecutionPeriod().getInitialDate());
+		
+		if (!allTasksAlreadySelected.isEmpty()) {
+			model.setAttribute("suggestedExecutionPeriod.finalDate", entity.getSuggestedExecutionPeriod().getInitialDate());
+		}
 		
 		request.unbind(entity, model, 
 			"title", 
